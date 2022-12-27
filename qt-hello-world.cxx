0174eba7312f6866ac6716309a89ac126cddf14e
Kashyap <kashyap@hipro.co.in>
2022-12-27 16:09:42 +0530

    Stay window on top

--- qt-hello-world.cxx
+++ qt-hello-world.cxx
@@ -25,6 +25,7 @@ int main(int argc, char *argv[ ])
 	QApplication app(argc, argv);
 	QLabel hello("<center>Welcome to my first Qt program</center>");
 	hello.setWindowTitle("My First Qt Program");
+	hello.setWindowFlags(Qt::WindowStaysOnTopHint);
 	hello.resize(400, 400);
 	hello.show();
 	return app.exec();
